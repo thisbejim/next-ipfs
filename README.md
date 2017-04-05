aed76623928d47bb843c47332931f8306a1eb7c2
James Childs-Maidment <jimmy_cm3@hotmail.com>
2017-04-05 15:43:49 +1000

    details

---
+++ README.md
@@ -0,0 +1,8 @@
+# next-ipfs
+
+1. Install ipfs: https://ipfs.io/docs/install/
+2. `export IPFS_PATH=.ipfs`
+3. `ipfs init`
+4. Run the daemon: `API_ORIGIN="*" ipfs daemon`
+5. `npm install`
+6. `npm run dev`
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
 import ipfsAPI from 'ipfs-api';
+import concat from 'concat-stream';
+
+// ipfs works on the server but not on the client
+// checkout https://github.com/VictorBjelkholm/webpack-node-ipfs-api-demo
 const ipfs = ipfsAPI('localhost', '5001');
-export default () => {
-  return (
-    <div>hello</div>
-  )
+
+export default class extends React.Component {
+  static async getInitialProps ({ req }) {
+    // save some data
+    const data = 'world';
+    const buffer = new Buffer(JSON.stringify(data))
+    const obj = await ipfs.add(buffer);
+    const hash = obj[0].hash;
+
+    // retrieve it
+    const stream = await ipfs.cat(hash);
+    const message = await new Promise((resolve) => stream.pipe(concat(data => resolve(JSON.parse(data)))));
+    return { message };
+  }
+  render () {
+    return(
+      <h1>hello {this.props.message}</h1>
+    )
+  }
 }
